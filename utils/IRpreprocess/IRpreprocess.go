8798823aeeafdbd2e1894e2ccb0bc4a817c3bbd2
lqw <lqw332664203@mail.ustc.edu.cn>
2023-04-12 14:33:49 +0800

    IR预处理添加attribute删除、metadata定义删除、等于函数删除

--- utils/IRpreprocess/IRpreprocess.go
+++ utils/IRpreprocess/IRpreprocess.go
@@ -116,18 +116,22 @@ func surroundedByQuotes(idx int, quotes []QuotePair) bool {
 	return false
 }
 
-type ModuleASMDeletor struct{}
-
-func (ModuleASMDeletor) run(content string) string {
-	lines := strings.Split(content, "\n")
-	res := make([]string, 0, len(lines))
+func excludeRegexMatch(lines []string, regex string) (res []string) {
+	res = make([]string, 0, len(lines))
+	re := regexp.MustCompile("(?m)" + regex)
 	for _, line := range lines {
-		re := regexp.MustCompile("module asm .*")
 		if !re.MatchString(line) {
 			res = append(res, line)
 		}
 	}
-	return strings.Join(res, "\n")
+	return res
+}
+
+type ModuleASMDeletor struct{}
+
+func (ModuleASMDeletor) run(content string) string {
+	lines := strings.Split(content, "\n")
+	return strings.Join(excludeRegexMatch(lines, "^module asm .*"), "\n")
 }
 
 type LabelPrefixTrimer struct{}
@@ -159,6 +163,73 @@ func phiTrimLabel(line string) string {
 	return line
 }
 
+type AttributeDeleter struct{}
+
+func (AttributeDeleter) run(content string) string {
+	lines := strings.Split(content, "\n")
+	return strings.Join(excludeRegexMatch(lines, "^attributes.*"), "\n")
+}
+
+type MetaDataDefineDeleter struct{}
+
+func (MetaDataDefineDeleter) run(content string) string {
+	lines := strings.Split(content, "\n")
+	return strings.Join(excludeRegexMatch(lines, "^!\\d+.*"), "\n")
+}
+
+type EqualFuncDeleter struct{}
+
+func (EqualFuncDeleter) run(content string) string {
+	lines := strings.Split(content, "\n")
+	ranges := findAllEqualFunc(lines)
+	return strings.Join(removeRanges(lines, ranges), "\n")
+}
+
+// Range is [startLine, endLine)
+type Range struct {
+	startLine int
+	endLine   int
+}
+
+// remove ranges in lines
+// if ranges = [s1, e1) [s2, e2) then res is [0, s1) [e1, s2)
+func removeRanges(lines []string, ranges []Range) (res []string) {
+	res = make([]string, 0, len(lines)/2)
+	len_ranges := len(ranges)
+	if len_ranges == 0 {
+		return lines
+	}
+	res = append(res, lines[0:ranges[0].startLine]...)
+	for i, _ := range ranges {
+		if i == len(ranges)-1 {
+			break
+		}
+		res = append(res, lines[ranges[i].endLine:ranges[i+1].startLine]...)
+	}
+	res = append(res, lines[ranges[len_ranges-1].endLine:]...)
+	return res
+}
+
+func findAllEqualFunc(lines []string) (res []Range) {
+	res = make([]Range, 0, len(lines)/10)
+	isEqualNow := false
+	var tmpRange Range
+	startFuncRe := regexp.MustCompile("define.*?@.*?\\.\\.eq.*")
+	endFuncRe := regexp.MustCompile("^\\}.*")
+	for i, line := range lines {
+		if !isEqualNow && startFuncRe.MatchString(line) {
+			isEqualNow = true
+			tmpRange.startLine = i
+		}
+		if isEqualNow && endFuncRe.MatchString(line) {
+			isEqualNow = false
+			tmpRange.endLine = i + 1
+			res = append(res, tmpRange)
+		}
+	}
+	return res
+}
+
 func init() {
 	flag.Usage = Usage
 }
@@ -179,8 +250,20 @@ func main() {
 	mgr.addPass(ModuleASMDeletor{})
 	mgr.addPass(CommentDeleter{})
 	mgr.addPass(LabelPrefixTrimer{})
+	mgr.addPass(AttributeDeleter{})
+	mgr.addPass(MetaDataDefineDeleter{})
+	mgr.addPass(EqualFuncDeleter{})
 	mgr.runAll(&content)
-	fmt.Println(content)
+	writeResult(content, "preprocessed."+file)
+}
+
+func writeResult(content string, file string) {
+	f, err := os.Create(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	fmt.Fprintln(f, content)
 }
 
 func readContent(file string) string {
